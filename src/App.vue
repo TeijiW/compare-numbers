bc781d6626f99d700e21db02dcfcda3ad1534fe3
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-03-01 23:34:32 -0400

    Reorganizing CSS

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <template>
-  <div class="w-full flex flex-col items-center mt-24">
-    <h1 class="font-bold text-7xl mb-8">Comparação de números</h1>
+  <div class="main-container mt-24">
+    <h1 class="title-1 mb-8">Comparação de números</h1>
 
-    <h2 class="font-bold text-3xl text-gray-600 mb-2 mt-8">
-      Base da comparação
-    </h2>
+    <h2 class="title-2 mb-2 mt-8">Base da comparação</h2>
 
-    <div
-      class="
-        flex flex-col
-        items-center
-        w-3/5
-        border-4
-        rounded-lg
-        border-black
-        py-4
-        px-2
-      "
-      style="min-height: 250px"
-    >
+    <div class="base-board">
       <NewNumberInput @done="insertNumber($event)" class="mb-2" />
       <BaseNumbersBoard :numbers="numbers" @remove="removeNumber($event)" />
     </div>
